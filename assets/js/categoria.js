e1b76876057ccf8e777744e5c37a1bca898bf752
Murilo Menezes <murilomenezes@Murilos-MacBook-Pro.local>
2019-09-18 03:15:42 -0300

    adding files

---
+++ assets/js/categoria.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let db = coDesConnect('https://portfolio-19d97.firebaseio.com/')
+
+  db.download('/', function(data) {
+    context = data['projetos']
+    console.log(context)
+    coDesReplace('.header', context)
+    coDesReplace('.parent', context)
+
+   
+  })
+})
